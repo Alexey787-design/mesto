13f8d6477e36cb71e63d50dc5b6298a0603875a0
Alexey S <alexey787@ya.ru>
2022-05-07 08:16:44 +1000

    do js validate v4

--- scripts/validate.js
+++ scripts/validate.js
@@ -40,8 +40,6 @@ function hasInvalidInput(inputList) {
   })
 }
 
-
-//добавить и при открытии попапа 
 function toggleButton(inputList, config) {
   if (hasInvalidInput(inputList)) {
     disableSubmitButton(config)
